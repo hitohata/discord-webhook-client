5e3fa5c92d25944adced3e51ca1e49a779235ace
hirohata <hirohatatro@gmail.com>
2024-03-03 22:37:43 -0800

    can call webhook

---
+++ README.md
@@ -0,0 +1,8 @@
+# discord webhook client
+
+This is just a client of Discord Webhook.
+
+## prerequisite
+
+You must have a Discord Webhook URI.
+Set it as an environment value. Its key is "URI".
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde_json::json;
+
+#[tokio::main]
+async fn main() {
+
+    let url = std::env::var("URI").unwrap();
+
+    let client = reqwest::Client::new();
+
+    let body = json!({
+        "content": "Hello from Rust\nHow are you?"
+    });
+
+    let res = client.post(url)
+        .header("Content-Type", "application/json")
+        .body(body.to_string())
+        .send()
+        .await;
+
+    match res {
+        Ok(r) => println!("{:?}", r),
+        Err(e) => println!("{:?}", e)
+    }
 }
